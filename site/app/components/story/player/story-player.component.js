843f6be4eb9935e162725b7deb5cc96739a2c205
Jared <jaredgglass@gmail.com>
2017-01-24 17:54:59 +0000

    story player tempalte fix

--- site/app/components/story/player/story-player.component.js
+++ site/app/components/story/player/story-player.component.js
@@ -1,16 +1,11 @@
 angular
 	.module('storyGameMaker')
 	.component('storyPlayer', {
-		templateUrl: "app/components/story/player/player.html",
+		templateUrl: "app/components/story/player/story-player.html",
 		bindings: {
 			story: "<"
 		},
 		controller: function(StoryService) {
 			var ctrl = this;
-
-			// init
-			ctrl.$onInit = function() {
-
-			}
 		}
 	});
