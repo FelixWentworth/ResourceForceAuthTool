76d5db822551f70afccc8bfb09f40feef88e6675
Jared <jaredgglass@gmail.com>
2017-02-13 13:38:19 +0000

    validated input component

---
+++ site/app/components/validated-input/validated-text-input.component.js
@@ -0,0 +1,10 @@
+angular
+	.module("storyGameMaker")
+	.component("validatedTextInput", {		
+		templateUrl: "app/components/validated-input/validated-text-input.html",
+		bindings: {
+			ngModel: "=",
+			ngMinlength: "<",
+			ngRequired: "<"
+		}
+	});
