b6f09ed7d5a4c1742cd6fa44b2995e3306e66cdb
Jared <jaredgglass@gmail.com>
2017-02-14 18:24:26 +0000

    choice editor started

---
+++ site/app/modules/story/components/tree-editor/choice/story-tree-choice-editor.component.js
@@ -0,0 +1,8 @@
+angular
+	.module("story")
+	.component("storyTreeChoiceEditor", {
+		templateUrl: "app/modules/story/components/tree-editor/choice/story-tree-choice-editor.html",
+		bindings: {
+			choice: "="
+		}
+	});
