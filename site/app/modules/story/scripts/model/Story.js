99242acc49f9231c4865786f24f0d85b89fecd14
Jared <jaredgglass@gmail.com>
2017-02-15 18:19:43 +0000

    create story started

--- site/app/modules/story/scripts/model/Story.js
+++ site/app/modules/story/scripts/model/Story.js
@@ -7,9 +7,5 @@ class Story {
 		self.metadata = new Metadata();
 		self.characters = [];
 		self.scene = new Scene();
-
-		// Stories root scene must have at least one character and one choice
-		self.characters.push(new Character());
-		self.scene.choices.push(new Choice());
 	}
 }
