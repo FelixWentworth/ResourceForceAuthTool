2472b9b26a0e72a672326f96e9d10a1d99f878ce
Jared <jaredgglass@gmail.com>
2018-04-26 10:42:13 +0100

    version 0 and web api controller

---
+++ server/PlayGen.ResourceForceAuthoringTool.Core/Version.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlayGen.ResourceForceAuthoringTool.Core
+{
+    public static class Version
+    {
+        public const int Major = 0;
+
+        public const int Minor = 0;
+
+        public const int Build = 0;
+        
+        public static string Full => $"{Major}.{Minor}.{Build}";
+
+        public static bool IsMajorVersionMatch(string checkVersion) => int.Parse(checkVersion.Split('.')[0]) == Major;
+    }
+}
---
+++ server/PlayGen.ResourceForceAuthoringTool.WebAPI/VersionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlayGen.ResourceForceAuthoringTool.WebAPI
+{
+    [Route("api/[controller]")]
+    public class VersionController
+    {
+        [HttpGet]
+        public IActionResult GetVersion() => new ObjectResult(Core.Version.Full);
+    }
+}
