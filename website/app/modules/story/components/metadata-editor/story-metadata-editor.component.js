d91f03b826326d68a89180364874648b6715e27b
Ellis Spice <ellis@playgen.com>
2018-04-30 16:57:41 +0100

    Users can now only select locations/languages their profile has access to when creating content. Location and language auto-selected if only one available.

--- website/app/modules/story/components/metadata-editor/story-metadata-editor.component.js
+++ website/app/modules/story/components/metadata-editor/story-metadata-editor.component.js
@@ -5,8 +5,11 @@ angular
 		bindings: {
 			metadata: "="
 		},
-		controller: [ "config", function(config) {
+		controller: [ "config", "$http", "Auth", "$q", function(config, $http, Auth, $q) {
 			var ctrl = this;
+
+			ctrl.isLoggedIn = Auth.isLoggedIn();
+			ctrl.userAllowedLocations = ctrl.isLoggedIn ? Auth.getAllowedLocations() : "";
 
 			ctrl.metadataHelp = {
 				"title": "",
@@ -14,10 +17,29 @@ angular
 				"language": "",				
 			};
 
-			ctrl.location = config.content.locations;
-			ctrl.language = config.content.languages;
+			ctrl.allowedLocations = {};
+			ctrl.locations = [];
+			ctrl.languages = ctrl.metadata.location != null ? ctrl.allowedLocations[ctrl.metadata.location] : [];
 			
 			ctrl.titleMin = config.constraints.title.min;
 			ctrl.titleMax = config.constraints.title.max;
+
+			ctrl.$postLink = function() {
+				if (ctrl.userAllowedLocations != null && ctrl.userAllowedLocations != "")
+                {
+					ctrl.allowedLocations = JSON.parse(ctrl.userAllowedLocations);
+					ctrl.locations = Object.keys(ctrl.allowedLocations)
+					if (ctrl.locations.length == 1) {
+						ctrl.metadata.location = ctrl.locations[0];
+					}
+                }
+			}
+
+			ctrl.changeLocation = function() {
+				ctrl.languages = ctrl.allowedLocations[ctrl.metadata.location];
+				if (ctrl.languages.length == 1) {
+					ctrl.metadata.language = ctrl.languages[0];
+				}
+			}
 		}]
 	});
