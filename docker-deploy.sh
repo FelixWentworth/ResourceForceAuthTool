2cd0b17ff1a71b3a09a90f2cc4dfabaa2c9890de
BAYMAX\Felix <felix@playgen.com>
2018-05-10 16:03:47 +0100

    Add initial migration remove inspect locations set to v1

--- docker-deploy.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-export DB_CONTAINER="resourceforceauthoringtool-mariadb"
-export CONTAINER="resourceforceauthoringtool"
-docker stop $CONTAINER
-docker stop $DB_CONTAINER
-docker rm $CONTAINER
-docker rm $DB_CONTAINER
-docker-compose up -d
--- server/PlayGen.ResourceForceAuthoringTool.WebAPI/delay-startup.sh
+++ server/PlayGen.ResourceForceAuthoringTool.WebAPI/delay-startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -z ${RESOURCEFORCEAUTHORINGTOOL_v0_MARIADB_PORT_3306_TCP} ]; then
+if [ -z ${RESOURCEFORCEAUTHORINGTOOL_V1_MARIADB_PORT_3306_TCP} ]; then
 	echo "You must link this container with mariadb first"
 	exit 1
 fi
         
-until nc -w 1 -z resourceforceauthoringtool-v0-mariadb 3306; do
+until nc -w 1 -z resourceforceauthoringtool-v1-mariadb 3306; do
 	echo "$(date) - waiting for resourceforceauthoringtool-mariadb..."
 	sleep 1
 done
