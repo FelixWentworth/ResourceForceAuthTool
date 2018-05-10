c179c19a6673e49f0611fbce5d74b0ee51683007
BAYMAX\Felix <felix@playgen.com>
2018-05-10 16:58:11 +0100

    update app settings connection path

--- server/PlayGen.ResourceForceAuthoringTool.WebAPI/delay-startup.sh
+++ server/PlayGen.ResourceForceAuthoringTool.WebAPI/delay-startup.sh
@@ -6,7 +6,7 @@ if [ -z ${RESOURCEFORCEAUTHORINGTOOL_V1_MARIADB_PORT_3306_TCP} ]; then
 fi
         
 until nc -w 1 -z resourceforceauthoringtool-v1-mariadb 3306; do
-	echo "$(date) - waiting for resourceforceauthoringtool-mariadb..."
+	echo "$(date) - waiting for resourceforceauthoringtool-v1-mariadb..."
 	sleep 1
 done
 
